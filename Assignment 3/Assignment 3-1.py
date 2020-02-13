4df895955bb96fc9fa7c7b613ee6f954879b2e07
B-kelly28 <60242328+B-kelly28@users.noreply.github.com>
2020-02-13 16:05:19 -0600

    Add files via upload

---
+++ Assignment 3/Assignment 3-1.py
@@ -0,0 +1,5 @@
+﻿# This program will convert between fahrenheit and celcius.
+print("Enter Fahrenheit temperature:")
+fahrenheit = float(input())
+celcius = (fahrenheit - 32) * 5 / 9
+print(str(fahrenheit) + "° Fahrenheit is " + str(celcius) + "° Celcius")
---
+++ Assignment 3/Assignment 3-2.py
@@ -0,0 +1,5 @@
+﻿# This program will convert between Celcius and Fahrenheit
+print("Enter Celcius temperature:")
+celcius = float(input())
+fahrenheit = celcius * 9 / 5 + 32
+print(str(celcius) + "° Celcius is " + str(fahrenheit) + "° Fahrenheit")
